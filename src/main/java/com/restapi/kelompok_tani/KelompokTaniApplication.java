e64470878c11b92deb41f519b10fc4a2939d834a
M Hamdan Syakirin <900373@HQ.bni.co.id>
2024-11-04 13:42:41 +0700

    new scrip projek kelompok tani

---
+++ src/main/java/com/restapi/kelompok_tani/KelompokTaniApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.kelompok_tani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelompokTaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelompokTaniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/kelompok_tani/KelompokTaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.kelompok_tani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelompokTaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
